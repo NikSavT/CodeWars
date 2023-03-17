4ff603bffc965e10e63c0eb59d5806fa6e5d2a0c
NikSavT <hickhickert@gmail.com>
2023-03-17 16:21:48 +0300

    feat: add samples

---
+++ 7kyu/Handshake problem.js
@@ -0,0 +1,37 @@
+// // DESCRIPTION:
+// Johnny is a farmer and he annually holds a beet farmers convention "Drop the beet".
+
+// Every year he takes photos of farmers handshaking. Johnny knows that no two farmers handshake more than once. 
+// He also knows that some of the possible handshake combinations may not happen.
+// However, Johnny would like to know the minimal amount of people that participated this year just by counting all the handshakes.
+
+// Help Johnny by writing a function, that takes the amount of handshakes and returns the minimal amount of people 
+// needed to perform these handshakes (a pair of farmers handshake only once).
+
+// Джонни — фермер, и он ежегодно проводит съезд фермеров-свекловодов «Брось свеклу». Каждый год он фотографирует рукопожатие фермеров. 
+// Джонни знает, что никакие два фермера не пожимают друг другу руки больше одного раза. 
+// Он также знает, что некоторые из возможных комбинаций рукопожатий могут не состояться. 
+// Однако Джонни хотел бы узнать минимальное количество людей, участвовавших в этом году, просто подсчитав все рукопожатия. 
+// Помогите Джонни, написав функцию, которая считает количество рукопожатий и возвращает минимальное количество людей, 
+// необходимое для выполнения этих рукопожатий (пара фермеров рукопожатие только один раз).
+
+// //------------------------------ My Solution ------------------------------
+
+function getParticipants(handshakes){
+  let handshakesCount = 0;
+while (handshakesCount < handshakes) { // выводит 0, затем 1, затем 2
+  handshakesCount+=
+  i++;
+}
+  // return 0
+}
+
+// //------------------------------Solution 1------------------------------
+
+
+
+// //------------------------------Solution 2------------------------------
+
+
+let str = '"102269"'
+console.log(getParticipants(str));//"12222666666999999999"
